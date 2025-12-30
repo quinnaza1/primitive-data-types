4b4189f85f80b6f8782f50bec6075fa692b81362
quinnaza <ehinmosanayomide@gmail.com>
2025-12-30 15:56:57 +0100

    Completed javascripts booleans excercise

--- booleans/booleans.js
+++ booleans/booleans.js
@@ -6,7 +6,7 @@
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let newBoolean
+let newBoolean = true
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -25,9 +25,9 @@ let dadsHeight = generateRandomNumber(100, 200)
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let momIsTaller
-let dadIsTaller
-let areTheSameHeight
+let momIsTaller = momsHeight > dadsHeight
+let dadIsTaller = momsHeight < dadsHeight
+let areTheSameHeight = momsHeight == dadsHeight
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -46,7 +46,7 @@ let studentEssay = generateSudentEssay()
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let notPlagiarised
+let notPlagiarised = !studentEssay.toLowerCase().includes(keyPhrase)
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -68,10 +68,10 @@ let thresholdForTall = 150
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let siblingsAreTall
-let siblingsAreShort
-let atLeastOneIsTall
-let onlyOneIsTall
+let siblingsAreTall = sistersHeight > thresholdForTall && brothersHeight > thresholdForTall
+let siblingsAreShort = sistersHeight < thresholdForTall && brothersHeight < thresholdForTall
+let atLeastOneIsTall = sistersHeight > thresholdForTall || brothersHeight > thresholdForTall
+let onlyOneIsTall = (sistersHeight > thresholdForTall && brothersHeight < thresholdForTall) || (sistersHeight < thresholdForTall && brothersHeight > thresholdForTall)
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -93,8 +93,10 @@ let secretCode = generateSecretCode()
 //  numerous variables for each check, and combine them at the end
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
-
-let codeIsValid
+let correct5thchar = secretCode.charCodeAt(4) >= 65 && secretCode.charCodeAt(4) <= 90
+let correct10thchar = secretCode.charCodeAt(9) >= 48 && secretCode.charCodeAt(9) <= 57
+let is15char = secretCode.length === 15
+let codeIsValid = correct5thchar && correct10thchar && is15char
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -115,13 +117,13 @@ let truthy1, truthy2
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-// falsy1 = Boolean("")
-// falsy2 = Boolean(0)
-// falsy3 = Boolean(undefined)
-// falsy4 = Boolean(null)
+falsy1 = Boolean("")
+falsy2 = Boolean(0)
+falsy3 = Boolean(undefined)
+falsy4 = Boolean(null)
 
-// truthy1 = Boolean("non-empty string")
-// truthy2 = Boolean(1)
+truthy1 = Boolean("non-empty string")
+truthy2 = Boolean(1)
 
 ////////////////////////////////////////////////////////////////////////
 
